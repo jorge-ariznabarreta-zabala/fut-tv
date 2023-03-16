5cca803c29c2c1743a686b20c06e66653371d79d
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2023-03-16 13:43:24 +0100

    funciona axios

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+FOOTER
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+HEADER
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
