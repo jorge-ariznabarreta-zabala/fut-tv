b85bacbfb7e2461ca9ba40a82f2d02a356838460
anyell <122279317+anyellpenascal@users.noreply.github.com>
2023-03-16 10:57:57 +0100

    navbar component realizado.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import * as bootstrap from 'bootstrap'
+import './assets/scss/style.css'
 
 const app = createApp(App)
 
