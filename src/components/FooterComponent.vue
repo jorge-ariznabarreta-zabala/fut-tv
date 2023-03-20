65982879318aa6b92019039fdc851f1871001f57
ikatza1967 <ikatzacarbon1@gmail.com>
2023-03-20 10:01:53 +0100

    foter finalizado

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,12 +1,12 @@
 <template>
 
   <div></div>
-  <footer class="d-flex">
+  <footer class="d-flex mt-5">
     <div class="text-black-80 text-left ps-5 gap-3 w-75"> &#169; Football All rights reserved 2023 </div>
-      <div class="redes-container d-flex justify-content-sm-end pe-5 gap-3 w-100  ">
-        <img src="src/assets/scss/iconos/2175193.png" alt="twiter"/> 
-        <img src="../assets/scss/iconos/739244.png" alt="instagram"/> 
-        <img src="../assets/scss/iconos/25347.png" alt="facebook"/> 
+      <div class="redes-container d-flex justify-content-sm-end pe-5 gap-3 w-100 pb-3 ">
+        <img src="src/assets/iconos/2175193.png" alt="twiter"/> 
+        <img src="../assets/iconos/739244.png" alt="instagram"/> 
+        <img src="../assets/iconos/25347.png" alt="facebook"/> 
       </div>
   </footer> 
       
@@ -19,6 +19,6 @@
 <style scoped> 
  .redes-container{
   height: 50px;
-  padding: 0.4rem;
+  /* padding: 0.4rem; */
 }
 </style>
