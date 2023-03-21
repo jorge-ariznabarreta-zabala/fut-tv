0e0942c749b145782e56cb0389d7fc814c5735ff
Medibra322 <medibra322@gmail.com>
2023-03-21 12:41:08 +0100

    sacamos por pantalla los equipos con imagene

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,33 @@
 <script setup>
-import { RouterView } from 'vue-router';
+import { PATTERNLIKE_TYPES } from '@babel/types';
+import axios from 'axios'
+import path from 'path';
+import { ref } from 'vue'
+
+let callApiSoccer = {
+  url: 'https://livescore6.p.rapidapi.com/leagues/v2/get-table?Category=soccer&Ccd=spain&Scd=laliga-santander',
+  headers: {
+    'X-RapidAPI-Key': '2354e7edb9mshf8c9fa220c15434p1804fajsn9793ea2f18a3'
+  }
+}
+const equiposLiga = ref([])
+
+const getData = async () => {
+  try {
+    const { data } = await axios.request(callApiSoccer)
+    console.log(data.LeagueTable.L[0].Tables[0].team)
+    equiposLiga.value = data.LeagueTable.L[0].Tables[0].team
+  } catch (error) {
+    console.log(error)
+  }
+}
+getData()
 </script>
 
 <template>
-
+  <div>
+    <ul>
+      <li v-for="equipos in equiposLiga" key="index"> {{ equipos.Tnm }} --- {{ equipos.ptsn }}</li>
+    </ul>
+  </div>
 </template>
