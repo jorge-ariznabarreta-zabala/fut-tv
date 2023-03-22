bbe7d139b8d92db32be35c64f7f2fd83854563d9
Juan-Little <juanbenitodevalle@gmail.com>
2023-03-22 08:46:52 +0100

    pull mañanero sin cambios

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -4,7 +4,7 @@
 <template>
   <nav class="navbar navbar-expand-lg">
     <div class="container-fluid contenedor">
-      <a class="navbar-brand" href="#"><img src="../assets/image_1-removebg-preview_1_50.png" alt=""></a>
+      <a class="navbar-brand" href="#"><img src="src/assets/image/image_1-removebg-preview_1_50.png" alt=""></a>
       <button
         class="navbar-toggler"
         type="button"
@@ -17,7 +17,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class ="collapse navbar-collapse " id="navbarSupportedContent ">
-        <ul class="navbar-nav mb-2 mb-lg-0 d-flex flex-end me-5 ">
+        <ul class="navbar-nav mb-2 mb-lg-0 d-flex me-5">
           <li class="nav-item">
             <RouterLink to="/" class="nav-link">Home</RouterLink>
           </li>
@@ -59,8 +59,8 @@
 .nav {
   background-color: #ACD6D4;
 }
-.navbar-nav{ 
+.navbar-nav {
   flex-grow: 0 !important;
 }
- 
+
 </style>
