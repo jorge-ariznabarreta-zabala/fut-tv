b48b68ebc2de48ce27d00f6abf20436158598a1d
anyell <122279317+anyellpenascal@users.noreply.github.com>
2023-03-22 08:38:29 +0100

    se esta trabajando en la vista de la clasificacion

--- src/views/PdivisionView.vue
+++ src/views/PdivisionView.vue
@@ -1,14 +1,35 @@
+<script setup>
+import TitlePages from '../components/TitlePages.vue';
+
+import axios from 'axios'
+import { ref } from 'vue'
+
+let callApiSoccer = {
+  url: 'https://livescore6.p.rapidapi.com/leagues/v2/get-table?Category=soccer&Ccd=spain&Scd=laliga-santander',
+  headers: {
+    'X-RapidAPI-Key': '2354e7edb9mshf8c9fa220c15434p1804fajsn9793ea2f18a3'
+  }
+}
+const equiposLiga = ref([])
+
+const getData = async () => {
+  try {
+    const { data } = await axios.request(callApiSoccer)
+    console.log(data.LeagueTable.L[0].Tables[0].team)
+    equiposLiga.value = data.LeagueTable.L[0].Tables[0].team
+  } catch (error) {
+    console.log(error)
+  }
+}
+getData()
+
+</script>
+
+
 <template>
     <div>
         <TitlePages title="Clasificaccion"/>
     </div>
+
+    
 </template>
-
-<script setup>
-import { RouterView } from 'vue-router';
-import TitlePages from '../components/TitlePages.vue';
-</script>
-
-<style lang="scss" scoped>
-
-</style>
