710c8bed7690394a23f4a867ccd6a587c2c51610
anyell <122279317+anyellpenascal@users.noreply.github.com>
2023-03-23 11:47:53 +0100

    se termino la vista de contacto

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,13 +1,155 @@
 <script setup>
-import { RouterView } from 'vue-router';
-import TitlePages from '../components/TitlePages.vue';
+import TitlePages from '../components/TitlePages.vue'
+
+function validateForm() {
+  var name = document.getElementById('name').value
+  if (name == '') {
+    document.querySelector('.status').innerHTML = alert('Name cannot be empty')
+    return false
+  }
+  var email = document.getElementById('email').value
+  if (email == '') {
+    document.querySelector('.status').innerHTML = alert('Email cannot be empty')
+    return false
+  } else {
+    var re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (!re.test(email)) {
+      document.querySelector('.status').innerHTML = alert('Email format invalid')
+      return false
+    }
+  }
+  var subject = document.getElementById('subject').value
+  if (subject == '') {
+    document.querySelector('.status').innerHTML = alert('Subject cannot be empty')
+    return false
+  }
+  var message = document.getElementById('message').value
+  if (message == '') {
+    document.querySelector('.status').innerHTML = alert('Message cannot be empty')
+    return false
+  }
+  document.querySelector('.status').innerHTML = alert('Sending...')
+}
+
+
+
 </script>
 
 <template>
-<TitlePages title="Contacto"/>
+  <TitlePages title="Contacto" />
 
+  <div class="contacto container">
+    <div class="mapa mt-5">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23203.64230036141!2d-2.5239120163551965!3d43.367503414434715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd4e26da4bdd072b%3A0xf70d22df22b24213!2sLequeitio%2C%20Vizcaya!5e0!3m2!1ses!2ses!4v1679564168839!5m2!1ses!2ses"
+        width="100%"
+        height="450"
+        style="border: 0"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </div>
+
+    <div class="formulario">
+      <!--Section description-->
+      <p class="text-center w-responsive mx-auto mb-5">
+        Do you have any questions? Please do not hesitate to contact us directly. Our team will come
+        back to you within a matter of hours to help you.
+      </p>
+
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-md-9 mb-md-0 mb-5">
+          <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+            <!--Grid row-->
+            <div class="row">
+              <!--Grid column-->
+              <div class="col-md-6">
+                <div class="md-form mb-0">
+                  <label for="name" class="">Nombre</label>
+                  <input type="text" id="name" name="name" class="form-control" />
+                </div>
+              </div>
+              <!--Grid column-->
+
+              <!--Grid column-->
+              <div class="col-md-6">
+                <div class="md-form mb-0">
+                  <label for="email" class="">Correo</label>
+                  <input type="text" id="email" name="email" class="form-control" />
+                </div>
+              </div>
+              <!--Grid column-->
+            </div>
+            <!--Grid row-->
+
+            <!--Grid row-->
+            <div class="row">
+              <div class="col-md-12">
+                <div class="md-form mb-0">
+                  <label for="subject" class="">Asunto</label>
+                  <input type="text" id="subject" name="subject" class="form-control mb-2" />
+                </div>
+              </div>
+            </div>
+            <!--Grid row-->
+
+            <!--Grid row-->
+            <div class="row">
+              <!--Grid column-->
+              <div class="col-md-12">
+                <div class="md-form">
+                  <label for="message">Mensaje</label>
+                  <textarea
+                    type="text"
+                    id="message"
+                    name="message"
+                    rows="2"
+                    class="form-control md-textarea"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+            <!--Grid row-->
+          </form>
+
+          <div class="text-center text-md-left mt-5">
+            <input type="submit" class="btn btn-primary" id="btn" @click="validateForm()">
+          </div>
+          <div class="status"></div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-3 text-center">
+          <ul class="list-unstyled mb-0">
+            <li>
+              <i class="fas fa-map-marker-alt fa-2x"></i>
+              <p>Lekeitio,Bizkaia, 48280</p>
+            </li>
+
+            <li>
+              <i class="fas fa-phone mt-4 fa-2x"></i>
+              <p>+ 34 643 353 431</p>
+            </li>
+
+            <li>
+              <i class="fas fa-envelope mt-4 fa-2x"></i>
+              <p>contact@fut-tv.com</p>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-
+<style scoped>
+.formulario {
+  width: 50%;
+  margin: 3rem auto;
+}
 </style>
