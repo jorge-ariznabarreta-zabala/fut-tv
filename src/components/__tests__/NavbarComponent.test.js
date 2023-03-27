52e8f01428c6a308854e7c411da06572cd6e3768
Medibra322 <medibra322@gmail.com>
2023-03-27 14:09:37 +0200

    testing

--- src/components/__tests__/NavbarComponent.test.js
+++ src/components/__tests__/NavbarComponent.test.js
@@ -3,7 +3,7 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 
 import NavbarComponent from '@/components/NavbarComponent.vue'
-
+import AboutComponent from '@/components/NavbarComponent.vue'
 
 
 describe('NavbarComponent', () => {
@@ -14,6 +14,10 @@ describe('NavbarComponent', () => {
 
   it("exite un routerlink", () => {
     const wrapper = mount(NavbarComponent)
+    expect(wrapper.find('RouterLink').exists()).toBe(true)
+  }),
+  it("exite un routerlink", () => {
+    const wrapper = mount(AboutComponent)
     expect(wrapper.find('RouterLink').exists()).toBe(true)
   })
 });
