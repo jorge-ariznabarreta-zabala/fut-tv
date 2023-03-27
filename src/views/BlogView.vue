a4ea14a9799761ebe724aea0115f19da6c3c96e1
anyell <122279317+anyellpenascal@users.noreply.github.com>
2023-03-27 12:23:23 +0200

    blog hecho un 50%

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,11 +1,38 @@
 <script setup>
+import axios from 'axios'
 import TitlePages from '../components/TitlePages.vue'
-import BlogComponent from '../components/BlogComponent.vue';
+import { ref } from 'vue'
 
+const datosPosts = ref([])
+
+var config = {
+  method: 'get',
+maxBodyLength: Infinity,
+  url: 'http://localhost:3000/posts',
+  headers: { }
+};
+
+const getData = async () => {
+  try {
+    const  response  = await axios.request(config)
+    datosPosts.value = response.data
+    console.log(response.data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+getData()
 </script>
 
 <template>
- <TitlePages title="Blog" />
- <BlogComponent />
+  <TitlePages title="Blog" />
+  <div class="post container" v-for="datos in datosPosts" :key="id">
+    <div class="info m-3">{{ datos.author }}</div>
 
+    <div class="content-post">
+      <h2 class="title mb-5"> {{ datos.title }}</h2>
+
+      <p class="text">{{ datos.content }}</p>
+    </div>
+  </div>
 </template>
--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -29,9 +29,6 @@ function redirectToPage(){
   router.push("/");
 
   nuevoUsuario();
-  
-
-
   validateForm();
  
 }
