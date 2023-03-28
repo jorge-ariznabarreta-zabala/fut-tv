ee7f767493663d7ef52d7bcef3852ed5df7d1dd3
Medibra322 <medibra322@gmail.com>
2023-03-28 12:26:16 +0200

    trestestes de h5

--- src/components/__tests__/AboutComponent.test.js
+++ src/components/__tests__/AboutComponent.test.js
@@ -3,9 +3,19 @@ import { mount } from '@vue/test-utils'
 import AboutComponent from '@/components/AboutComponent.vue'
 
 
-describe('TestesDeUnit', () => {
+describe('AboutComponent.vue', () => {
     it('renders', () => {
         const wrapper = mount(AboutComponent)
         expect(wrapper.exists()).toBe(true)
-    })
+    }),
+    it("mount component", async () => {
+        expect(AboutComponent).toBeTruthy();
+      
+        const wrapper = mount(AboutComponent, {
+          props: {
+            title: "Guess User Age App",
+          },
+        });
+        expect(wrapper.text()).toContain("Guess User Age App");
+      });
 });
