747b7e027c82041442a84d92cbac941a5e1cc20f
ikatza1967 <ikatzacarbon1@gmail.com>
2023-03-28 12:03:36 +0200

    id imag footerComponent y test

---
+++ src/components/__tests__/FooterTest.spec.js
@@ -0,0 +1,23 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import FooterComponent from '@/components/FooterComponent.vue'
+
+
+ describe('TestesDeUnit', () => {
+     it('3 iconos', () => {
+         const wrapper = mount( FooterComponent );
+         const iconos = wrapper.find('#icono')
+         expect(iconos).toBeTruthy()
+    });
+
+    
+
+
+
+describe('TestesDeUnit', () => {
+    it('renders', () => {
+        const wrapper = mount(FooterComponent)
+        expect(wrapper.exists()).toBe(true)
+    })
+});
+ })
