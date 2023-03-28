7941fb6da5728996ac09295dc9c59ab45d7f5a0e
mihaicce <cocea.mike@gmail.com>
2023-03-28 08:27:37 +0200

    commit despues de pull

--- src/components/TarjetaNoticias.vue
+++ src/components/TarjetaNoticias.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <ul class="row">
-            <li class="col-lg-6 justify-content-center" v-for="noticias in noticias" v-bind:key="noticias">
+            <li class="col-12 col-lg-6 justify-content-center" v-for="noticias in noticias" v-bind:key="noticias">
                 <div class="contenido">
                     <div class=" d-flex  align-items-center mt-5" style="width: 10rem;">
                         <div class="card-body">
