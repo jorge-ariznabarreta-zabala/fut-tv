4f0a47e14a7efa237923df79090314ef630e6768
anyell <122279317+anyellpenascal@users.noreply.github.com>
2023-03-28 12:58:47 +0200

    trabajando en formulario

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -1,20 +1,18 @@
 <script setup>
-import router from "@/router";
-import axios from "axios";
+import router from '@/router'
+import axios from 'axios'
 import { ref } from 'vue'
 
- const  nombre = ref('');
- const  apellido  = ref('');
- const  correo  = ref('');
- const  contraseña  = ref('');
- const  direccion  = ref('');
-
-
-
+const nombre = ref('')
+const apellido = ref('')
+const correo = ref('')
+const contraseña = ref('')
+const direccion = ref('')
 
 const nuevoUsuario = async () => {
+
   const url = 'http://localhost:3000/usuarios'
-   await fetch(url, {
+  await fetch(url, {
     method: 'POST',
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify({
@@ -27,83 +25,78 @@ const nuevoUsuario = async () => {
   })
 }
 
-function redirectToPage() {
-  router.push('/')
 
-  nuevoUsuario()
-  validateForm()
-}
 
-function validateForm() {
-  var name = document.getElementById('name').value
-  if (name == '') {
-    document.querySelector('.status').innerHTML = alert('Name cannot be empty')
-    return false
-  }
-  var email = document.getElementById('email').value
-  if (email == '') {
-    document.querySelector('.status').innerHTML = alert('Email cannot be empty')
-    return false
-  } else {
-    var re =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    if (!re.test(email)) {
-      document.querySelector('.status').innerHTML = alert('Email format invalid')
-      return false
-    }
-  }
-  var subject = document.getElementById('subject').value
-  if (subject == '') {
-    document.querySelector('.status').innerHTML = alert('Subject cannot be empty')
-    return false
-  }
-  var message = document.getElementById('message').value
-  if (message == '') {
-    document.querySelector('.status').innerHTML = alert('Message cannot be empty')
-    return false
-  }
-  document.querySelector('.status').innerHTML = alert('Sending...')
-}
+
 </script>
 
 <template>
-<div class="col-lg-8 mx-auto ">
+  <div class="formularioRegistro">
+    <div class="formularios">
+    <FormKit
+      type="text"
+      label="Usuario"
+      name="usuario"
+      v-model="usuario"
+      validation="required|length:4"
+      validation-messages="este campo es obligatorio"
 
-<div id="containerRegister">
-  <div class=" datospersonales bd-exampledropdown-menu mx-auto form-color col-lg-7 col-md-8 m-5" >
-    <form class="px-5 py-3 needs-validation "  id="formulario2">
-      <h3 class="text-center pb-3">Registro</h3>
-      <div class="mb-3 ">
-          <label for="DropdownFormName1" class="form-label">Nombre</label>
-          <input type="text" class="form-control" id="name" v-model="nombre">
-      </div>
-      <div class="mb-3">
-          <label for="DropdownFormlastname" class="form-label">Apellido</label>
-          <input type="text" class="form-control" id="lastname" v-model="apellido">
-      </div>
-      <div class="mb-3">
-          <label for="DropdownFormdirection" class="form-label">Direccion</label>
-          <input type="text" class="form-control" id="address" v-model="direccion">
-      </div>
-      <div class="mb-3">
-        <label for="DropdownFormEmail1" class="form-label">Correo Electronico</label>
-        <input type="email" class="form-control" id="email" v-model="correo">
-      </div>
-      <div class="mb-3">
-        <label class="form-label">Contraseña</label>
-        <input type="email" class="form-control" id="password" v-model="contraseña">
-      </div>
-      <div class="mb-3 d-grid gap-2 col-6 mx-auto">
-        <button class="btn btn-outline-primary" @click="redirectToPage()">Registrate</button>
-      </div>
-    </form>
+    />
+
+    <FormKit
+      type="text"
+      label="Nombre Completo"
+      name="nombre"
+      v-model="nombre"
+      validation="required|length:4"
+      validation-messages="este campo es obligatorio"
+
+
+    />
+
+    <FormKit
+      type="email"
+      label="Correo"
+      name="correo"
+      v-model="correo"
+      validation="required|Correo"
+      validation-messages="este campo es obligatorio"
+
+    />
+
+    <FormKit
+      type="password"
+      name="Contraseña"
+      v-model="contraseña"
+      label="Password"
+      validation="required"
+      validation-visibility="live"
+      validation-messages="este campo es obligatorio"
+    />
+  
+    <FormKit 
+     type="submit"
+      label="registrarte"
+       name="enviar"
+       @click="nuevoUsuario" 
+       />
+
   </div>
-</div>
-</div>
+  </div>
 </template>
 
 <style scoped>
-#containerRegister {
+.formularioRegistro {
+  padding: 5rem 0;
   background-color: #acd6d4;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.formularios{
+  background-color: rgb(252, 250, 247);
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 6rem;
 }
 </style>
