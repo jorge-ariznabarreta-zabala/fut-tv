8a9907c59ab9518219545cc5e337562aae8109dd
Medibra322 <medibra322@gmail.com>
2023-03-30 10:44:09 +0200

    vitest span

--- src/components/__tests__/BlogComponent.test.js
+++ src/components/__tests__/BlogComponent.test.js
@@ -7,5 +7,10 @@ describe('TestesDeUnit', () => {
     it('renders', () => {
         const wrapper = mount(BlogComponent)
         expect(wrapper.exists()).toBe(true)
+    }),
+    it ('testeamos un parafo', () => {
+        const wrapper = mount (BlogComponent)
+        const patata = wrapper.get('p')
+        expect(patata.text()).toBe('This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.')
     })
 });
