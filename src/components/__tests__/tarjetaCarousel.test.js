c0d64cea5af7e922f5a52efd8ffecf72ce5e4ea9
Medibra322 <medibra322@gmail.com>
2023-03-31 11:55:20 +0200

    teste

--- src/components/__tests__/tarjetaCarousel.test.js
+++ src/components/__tests__/tarjetaCarousel.test.js
@@ -6,6 +6,10 @@ describe ('tarjetaCarousel', () => {
     it ('testeamos un span', () => {
         const wrapper = mount(tarjetaCarousel)
         expect(wrapper.find('span').exists()).toBe(true)
+    }),
+    it ('testeamos un span', () => {
+        const wrapper = mount(tarjetaCarousel)
+        expect(wrapper.find('button').exists()).toBe(true)
     })
 });
     
