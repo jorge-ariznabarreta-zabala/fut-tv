76335200b0945de8bb3c4697879364d311da96c9
anyell <122279317+anyellpenascal@users.noreply.github.com>
2023-03-31 09:18:24 +0200

    .

--- src/components/TarjetaNoticias.vue
+++ src/components/TarjetaNoticias.vue
@@ -32,22 +32,6 @@ import { ref } from 'vue'
 import TitlePages from '../components/TitlePages.vue'
 import TarjetaNoticias from '../components/TarjetaNoticias.vue'
 
-// defineProps ({
-//     noticias: {
-//         type: String,
-//         default: ''
-//     }
-// })
-//  const linc = [{
-//  link: 'https://as.com/futbol/mercado-de-fichajes-en-directo-altas-bajas-y-ultima-hora-hoy-n-43/?noti=mercado_mod_port',
-//  foto: "",
-//  name: 'Aprobados y suspensos del Barcelona: Sergi Roberto y Kessié, ‘héroes’ improvisados'},
-//  {link:'https://as.com/futbol/primera/al-son-de-griezmann-n/',
-//  foto: "",
-//  name: 'El Real Madrid ‘amarga’ la noche a Piqué'},
-//  {link:'https://as.com/futbol/primera/aubameyang-vuelve-a-un-clasico-n/',
-//  foto: "",
-//  name: 'Araújo rompe la racha de Ter Stegen en el Camp Nou: ¡1.124 minutos sin encajar un gol!'}]
 
 const primerLink = 'https://www.livescore.com'
 
